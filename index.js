035386e639002ed0f65f64f14daafc19cbcba198
Horváth Márk <horvath.mark@lightport.hu>
2020-04-05 23:01:52 +0200

    Fixed async error

--- index.js
+++ index.js
@@ -2,13 +2,49 @@ const express = require('express');
 const mongoose = require('mongoose');
 const http = require('http');
 
+
+
 const app = express();
 
 // Static serve
 const root = require('path').join(__dirname, 'client', 'build')
 app.use(express.static(root));
 
+// Use demo data
+const fs = require("fs");
+var users;
 
+// Get user list
+app.get('/api/users', (req, res) => {
+    res.json(users);
+});
+
+// Get specific user
+app.get('/api/user/:id', (req, res) => {
+    if (req.params.id >= users.length) res.send('User not found!');
+
+    res.json(users[req.params.id]);
+});
+
+readJSONFile('data/users.json', function (err, json) {
+    if (err) { throw err; }
+    users = json;
+});
+
+function readJSONFile(filename, callback) {
+    fs.readFile(filename, function (err, data) {
+        if (err) {
+            callback(err);
+            return;
+        }
+        try {
+            console.log("data loaded!");
+            callback(null, JSON.parse(data));
+        } catch (exception) {
+            callback(exception);
+        }
+    });
+}
 
 //Routes
 app.use(require('./routes'));
--- routes.js
+++ routes.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var util = require('./util.js');
 
-var users = util.invokeJsonData();
 
 //Middle ware that is specific to this router
 router.use(function timeLog(req, res, next) {
@@ -15,18 +13,6 @@ router.get('/api/getList', (req,res) => {
     var list = ["item1", "item2", "item3"];
     res.json(list);
     console.log('Sent list of items');
-});
-
-// Get user list
-router.get('/api/users', (req, res) => {
-    res.json(users);
-});
-
-// Get specific user
-router.get('/api/user/:id', (req, res) => {
-    if (req.params.id >= users.length) res.send('User not found!');
-
-    res.json(users[req.params.id]);
 });
 
 
