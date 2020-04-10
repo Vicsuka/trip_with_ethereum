28dba3a3513137b9ab9f6e814e6997035d38e6ce
vicsuka <horvath.mark@lightport.hu>
2020-04-10 12:58:42 +0200

    Authorization v1 complete

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/**
+ * Required External Modules
+ */
+
 const express = require('express');
 const mongoose = require('mongoose');
 const http = require('http');
@@ -5,15 +9,110 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const bodyParser = require('body-parser');
 
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+
+require("dotenv").config();
+
+const authRouter = require("./auth");
+
+/**
+ * App Variables
+ */
+
 const app = express();
 
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Static serve
+
+/**
+ * Session Configuration
+ */
+
+const session = {
+    secret: "nagyontitkos",
+    cookie: {},
+    resave: false,
+    saveUninitialized: false
+  };
+  
+if (app.get("env") === "production") {
+    // Serve secure cookies, requires HTTPS
+    session.cookie.secure = true;
+}
+
+/**
+ * Passport Configuration
+ */
+
+const strategy = new Auth0Strategy(
+    {
+      domain: process.env.AUTH0_DOMAIN,
+      clientID: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      callbackURL:
+        process.env.AUTH0_CALLBACK_URL || "http://localhost:5000/callback"
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+      /**
+       * Access tokens are used to authorize users to an API
+       * (resource server)
+       * accessToken is the token to call the Auth0 API
+       * or a secured third-party API
+       * extraParams.id_token has the JSON Web Token
+       * profile has all the information from the user
+       */
+      return done(null, profile);
+    }
+  );
+
+/**
+ *  App Configuration
+ */
+
 const root = require('path').join(__dirname, 'client', 'build')
 app.use(express.static(root));
+
+app.use(expressSession(session));
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+/**
+ * Routes Definitions
+ */
+
+const secured = (req, res, next) => {
+    console.log(req.user);
+    if (req.user) {
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+};  
+
+// Router mounting
+app.use("/", authRouter);
+
+app.get("/user", secured, (req, res, next) => {
+    const { _raw, _json, ...userProfile } = req.user;
+    res.render("user", {
+      title: "Profile",
+      userProfile: userProfile
+    });
+  });
 
 ////////////////////////////////////////////
 // Use demo data
