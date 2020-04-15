3a797c3e8592a756143cbe15342e38bb42af1f2b
vicsuka <vicsuka@gmail.com>
2020-04-15 21:53:44 +0200

    callback test

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const strategy = new Auth0Strategy(
       clientID: process.env.AUTH0_CLIENT_ID,
       clientSecret: process.env.AUTH0_CLIENT_SECRET,
       callbackURL:
-        process.env.AUTH0_CALLBACK_URL || "http://localhost:5000/callback"
+        process.env.AUTH0_CALLBACK_URL || "/callback"
     },
     function(accessToken, refreshToken, extraParams, profile, done) {
       /**
