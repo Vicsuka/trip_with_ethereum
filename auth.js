7a0af6991a64a6b185fd20055d01a7e3c635fff9
vicsuka <vicsuka@gmail.com>
2020-04-15 22:48:13 +0200

    Error messages provided

--- auth.js
+++ auth.js
@@ -30,9 +30,11 @@ router.get(
 router.get("/callback", (req, res, next) => {
     passport.authenticate("auth0", (err, user, info) => {
         if (err) {
+            console.log(err);
             return next(err);
         }
         if (!user) {
+            console.log(user);
             console.log("User login error");
             return res.redirect("/login");
         }
