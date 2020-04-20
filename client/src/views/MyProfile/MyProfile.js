f8864368be6d7de6ac92929636b255d6e7bdc051
vicsuka <vicsuka@gmail.com>
2020-04-20 23:19:47 +0200

    Design for user profile

--- client/src/views/MyProfile/MyProfile.js
+++ client/src/views/MyProfile/MyProfile.js
@@ -29,6 +29,9 @@ const styles = {
     fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
     marginBottom: "3px",
     textDecoration: "none"
+  },
+  aboutInfo: {
+    textAlign: "left"
   }
 };
 
@@ -39,6 +42,7 @@ export default function UserProfile() {
   const [profile, setProfile] = useState({});
 
   useEffect(() => {
+
     fetch("/api/userprofile")
       .then(response => response.json())
       .then(
@@ -50,28 +54,61 @@ export default function UserProfile() {
           console.log(error);
         }
       )
-   
+
   }, []);
 
-  
+
   return (
     <div>
       <GridContainer>
-        <GridItem xs={12} sm={12} md={4}>
+        <GridItem xs={12} sm={12} md={5}>
           <Card profile>
             <CardAvatar profile>
-                <img src={profile.picture} alt="..." />
+              <img src={profile.picture} alt="" />
             </CardAvatar>
-            <CardBody profile>
-              <h4 className={classes.cardTitle}>{profile.name}</h4>
-              <p className={classes.description}>
-                DESCRIPTION
-              </p>
+            <CardBody profile className={classes.aboutInfo}>
+              <CardHeader color="primary">
+                <h3 className={classes.cardTitleWhite + ' ' + classes.cardTitle}>{profile.name}</h3>
+                <p className={classes.cardCategoryWhite + ' ' + classes.description}>YOUR DATA</p>
+              </CardHeader>
+              <GridContainer >
+                <GridItem xs={12} sm={12} md={3}>
+                  <h4>Username:</h4>
+                </GridItem>
+                <GridItem xs={12} sm={12} md={9}>
+                  <h4>{profile.nickname}</h4>
+                </GridItem>
+              </GridContainer>
+              <GridContainer>
+                <GridItem xs={12} sm={12} md={3}>
+                  <h4>Email:</h4>
+                </GridItem>
+                <GridItem xs={12} sm={12} md={9}>
+                  <h4>{profile.email}</h4>
+                </GridItem>
+              </GridContainer>
+              <GridContainer>
+                <GridItem xs={12} sm={12} md={3}>
+                  <h4>ID:</h4>
+                </GridItem>
+                <GridItem xs={12} sm={12} md={9}>
+                  <h4>{profile.sub}</h4>
+                </GridItem>
+              </GridContainer>
+              <GridContainer>
+                <GridItem xs={12} sm={12} md={3}>
+                  <h4>About:</h4>
+                </GridItem>
+                <GridItem xs={12} sm={12} md={9}>
+                  <h4>{profile.about}</h4>
+                </GridItem>
+
+              </GridContainer>
             </CardBody>
           </Card>
         </GridItem>
 
-        <GridItem xs={12} sm={12} md={8}>
+        <GridItem xs={12} sm={12} md={7}>
           <Card>
             <CardHeader color="primary">
               <h4 className={classes.cardTitleWhite}>Edit Profile</h4>
@@ -85,8 +122,9 @@ export default function UserProfile() {
                     id="first-name"
                     formControlProps={{
                       fullWidth: true
-                    }}
-                  />
+                    }}>
+                    {profile.given_name}
+                  </CustomInput>
                 </GridItem>
                 <GridItem xs={12} sm={12} md={6}>
                   <CustomInput
@@ -105,30 +143,6 @@ export default function UserProfile() {
                     id="username"
                     formControlProps={{
                       fullWidth: true
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Your ID"
-                    id="user-id"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                    inputProps={{
-                      disabled: true
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Email address"
-                    id="email-address"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                    inputProps={{
-                      disabled: true
                     }}
                   />
                 </GridItem>
