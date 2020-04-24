ec620bd81c22548d40b5078c1fc28666be4318d6
vicsuka <vicsuka@gmail.com>
2020-04-24 16:09:57 +0200

    Mock data for testing

---
+++ client/mockdata.js
@@ -0,0 +1,34 @@
+var mockPerson = {
+    "address": {
+        "city": "NEW YORK",
+        "country": "USA",
+        "streetAddress": "8354 Woodland Drive",
+        "postalCode": "11795"
+    },
+    "_id": "5ea197ef8b66f04600f11664",
+    "auth0id": "testID",
+    "firstname": "John",
+    "lastname": "Black",
+    "username": "XjOhnX",
+    "email": "xjohnx@john.com",
+    "about": "I like to travel with random people!!",
+    "ethereumAddress": "0x83d768fcef9DEAFe715420E54b7072bc23aD0892",
+    "createdAt": "2020-04-23T13:28:15.377Z",
+    "updatedAt": "2020-04-23T13:28:15.377Z",
+    "__v": 0
+};
+
+var mockTrip = {
+    "_id": "5ea197ef8b66f04619f11664",
+    "title": "Test Trip",
+    "description": "<strong>Welcome to my Trip</strong>",
+    "organizerId": "testID",
+    "participantIds": ["testID"],
+    "price": 0.5,
+    "maxPersons": 2,
+    "smartContractAddress": "0x50dAdCb2AfE881445cE879789e484F82290790A0",
+    "smartContractType": "fullTrust",
+    "startingDate": "2020-04-23T13:28:15.377Z",
+    "deadLineDate": "2020-04-20T13:30:15.377Z",
+    "endingDate": "2020-04-28T13:30:15.377Z",
+};
