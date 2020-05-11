f5346ffc3c5f36de22e46e9eb414ca29c8b7da8d
vicsuka <vicsuka@gmail.com>
2020-05-11 23:52:19 +0200

    Basic data about trip

--- client/src/views/TripDetails/TripDetails.js
+++ client/src/views/TripDetails/TripDetails.js
@@ -14,7 +14,6 @@ import GlobalVariables from "variables/general.js";
 
 import Web3 from 'web3';
 
-import { bugs, website, server } from "variables/general.js";
 import Icon from "@material-ui/core/Icon";
 // @material-ui/icons
 import Store from "@material-ui/icons/Store";
@@ -22,24 +21,11 @@ import Warning from "@material-ui/icons/Warning";
 import DateRange from "@material-ui/icons/DateRange";
 import LocalOffer from "@material-ui/icons/LocalOffer";
 import Update from "@material-ui/icons/Update";
-import ArrowUpward from "@material-ui/icons/ArrowUpward";
-import AccessTime from "@material-ui/icons/AccessTime";
 import Accessibility from "@material-ui/icons/Accessibility";
-import BugReport from "@material-ui/icons/BugReport";
-import Code from "@material-ui/icons/Code";
-import Cloud from "@material-ui/icons/Cloud";
 // core components
-import Table from "components/Table/Table.js";
-import Tasks from "components/Tasks/Tasks.js";
-import CustomTabs from "components/CustomTabs/CustomTabs.js";
 import Danger from "components/Typography/Danger.js";
 import CardIcon from "components/Card/CardIcon.js";
 
-import {
-    dailySalesChart,
-    emailsSubscriptionChart,
-    completedTasksChart
-  } from "variables/charts.js";
   
 import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
 
@@ -195,72 +181,42 @@ export default function TripDetails(props) {
                                             <CardIcon color="warning">
                                                 <Icon>content_copy</Icon>
                                             </CardIcon>
-                                            <p className={classes.cardCategory}>Used Space</p>
-                                            <h3 className={classes.cardTitle}>
-                                                49/50 <small>GB</small>
-                                            </h3>
+                                            <p className={classes.cardCategory}>Status</p>
+                                            <h3 className={classes.cardTitle}></h3>
                                         </CardHeader>
-                                        <CardFooter stats>
-                                            <div className={classes.stats}>
-                                                <Danger>
-                                                    <Warning />
-                                                </Danger>
-                                                <a href="#pablo" onClick={e => e.preventDefault()}>
-                                                    Get more space
-                                                </a>
-                                            </div>
-                                        </CardFooter>
                                     </Card>
                                 </GridItem>
                                 <GridItem xs={12} sm={6} md={3}>
                                     <Card>
-                                        <CardHeader color="success" stats icon>
-                                            <CardIcon color="success">
+                                        <CardHeader color="warning" stats icon>
+                                            <CardIcon color="warning">
                                                 <Store />
                                             </CardIcon>
-                                            <p className={classes.cardCategory}>Revenue</p>
-                                            <h3 className={classes.cardTitle}>$34,245</h3>
+                                            <p className={classes.cardCategory}>Price</p>
+                                            <h3 className={classes.cardTitle}>{trip.price} Ξ</h3>
                                         </CardHeader>
-                                        <CardFooter stats>
-                                            <div className={classes.stats}>
-                                                <DateRange />
-                                                Last 24 Hours
-                                            </div>
-                                        </CardFooter>
                                     </Card>
                                 </GridItem>
                                 <GridItem xs={12} sm={6} md={3}>
                                     <Card>
-                                        <CardHeader color="danger" stats icon>
-                                            <CardIcon color="danger">
+                                        <CardHeader color="warning" stats icon>
+                                            <CardIcon color="warning">
                                                 <Icon>info_outline</Icon>
                                             </CardIcon>
-                                            <p className={classes.cardCategory}>Fixed Issues</p>
-                                            <h3 className={classes.cardTitle}>75</h3>
+                                            <p className={classes.cardCategory}>Contract type</p>
+                                            <h3 className={classes.cardTitle}>{trip.smartContractType}</h3>
                                         </CardHeader>
-                                        <CardFooter stats>
-                                            <div className={classes.stats}>
-                                                <LocalOffer />
-                                                Tracked from Github
-                                            </div>
-                                        </CardFooter>
                                     </Card>
                                 </GridItem>
                                 <GridItem xs={12} sm={6} md={3}>
                                     <Card>
-                                        <CardHeader color="info" stats icon>
-                                            <CardIcon color="info">
+                                        <CardHeader color="warning" stats icon>
+                                            <CardIcon color="warning">
                                                 <Accessibility />
                                             </CardIcon>
-                                            <p className={classes.cardCategory}>Followers</p>
-                                            <h3 className={classes.cardTitle}>+245</h3>
+                                            <p className={classes.cardCategory}>Participants</p>
+                                            <h3 className={classes.cardTitle}>{trip.participantIds ? trip.participantIds.length : "?"} / {trip.maxPersons}</h3>
                                         </CardHeader>
-                                        <CardFooter stats>
-                                            <div className={classes.stats}>
-                                                <Update />
-                                                Just Updated
-                                            </div>
-                                        </CardFooter>
                                     </Card>
                                 </GridItem>
                             </GridContainer>
