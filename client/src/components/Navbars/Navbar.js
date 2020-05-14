10832b10e180dc6d9453248a78dcf0250d50a3a4
vicsuka <vicsuka@gmail.com>
2020-05-14 23:08:52 +0200

    Fixed warning message for children property

--- client/src/components/Navbars/Navbar.js
+++ client/src/components/Navbars/Navbar.js
@@ -23,7 +23,7 @@ export default function Header(props) {
     var name;
     props.routes.map(prop => {
       if (window.location.href.indexOf(prop.layout + prop.path) !== -1) {
-        name = props.rtlActive ? prop.rtlName : prop.name;
+        name = prop.name;
       }
       return null;
     });
@@ -39,7 +39,7 @@ export default function Header(props) {
         <div className={classes.flex}>
           {/* Here we create navbar brand, based on route name */}
           <Button color="transparent" href="#" className={classes.title}>
-            {makeBrand()}
+            <div>{makeBrand()}</div>
           </Button>
         </div>
         <Hidden smDown implementation="css">
